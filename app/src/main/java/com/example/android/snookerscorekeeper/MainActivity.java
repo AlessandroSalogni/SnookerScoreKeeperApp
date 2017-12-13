f36eecfadfc040e7c77241c45986f26501c1e909
AlessandroSalogni <20013907@studenti.uniupo.it>
2017-12-13 16:23:12 +0100

    Complete app

--- app/src/main/java/com/example/android/snookerscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/snookerscorekeeper/MainActivity.java
@@ -1,146 +1,396 @@
 package com.example.android.snookerscorekeeper;
 
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String PLAYER_1 = "p1";
-    public static final String PLAYER_2 = "p2";
+    public static final String CURRENT_PLAYER = "current player";
+    public static final String CURRENT_POT_BALL = "current pot ball";
+    public static final String COLOUR_SEQUENCE = "colour_sequence";
+    public static final String NEXT_BALL_COLOUR_SEQUENCE = "next ball colour sequence";
+    public static final String CURRENT_PLAYER_COLOUR = "current player colour";
+
+    public static final int MAX_RED_BALL = 15;
+    public static final int START_SCORE = 0;
+    public static final int FOUL = -4;
 
     public enum Ball {
-        RED(1), YELLOW(2), GREEN(3), BROWN(4), BLUE(5), PINK(6), BLACK(7);
+        RED(1, "red"), YELLOW(2, "yellow"), GREEN(3, "green"), BROWN(4, "brown"), BLUE(5, "blue"), PINK(6, "pink"), BLACK(7, "black");
 
         int point;
+        String name;
 
-        Ball(int point) {
+        Ball(int point, String name) {
             this.point = point;
+            this.name = name;
         }
 
         public int getPoint() {
             return point;
         }
+
+        public String getName() {
+            return name;
+        }
+
+        public static Ball nextColourBall(Ball ball) {
+            switch(ball) {
+                case RED:
+                    return YELLOW;
+                case YELLOW:
+                    return GREEN;
+                case GREEN:
+                    return BROWN;
+                case BROWN:
+                    return BLUE;
+                case BLUE:
+                    return PINK;
+                case PINK:
+                    return BLACK;
+                default:
+                    return null;
+            }
+        }
     }
 
-    public enum Round {PLAYER_1, PLAYER_2}
+    public enum Round {PLAYER_1("p1"), PLAYER_2("p2");
+        String player;
 
-    private Round round;
+        Round(String player) {
+            this.player = player;
+        }
+
+        public String getPlayer() {
+            return player;
+        }
+    }
+
+    private Round currentPlayer;
     private Ball potBall;
-    private int redBall = 15;
-    private int scorePlayer1 = 0;
-    private int scorePlayer2 = 0;
+
+    private int redBall;
+    private int scorePlayer1;
+    private int scorePlayer2;
+    private int currentPlayerColour;
+
+    private Ball nextBallColourSequence;
+    private boolean colourSequence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        changeRound();
+
+        if(savedInstanceState == null)
+            initializeState();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        int id;
+
+        for (Round player : Round.values()) {
+            id = getResources().getIdentifier(player.getPlayer() + "_score", "id", getPackageName());
+            savedInstanceState.putCharSequence(String.valueOf(id), getScoreTextView(player.getPlayer()).getText());
+
+            id = getResources().getIdentifier(player.getPlayer() + "_foul", "id", getPackageName());
+            savedInstanceState.putCharSequence(String.valueOf(id), getFoulsTextView(player.getPlayer()).getText());
+
+            for (Ball ball : Ball.values()) {
+                id = getResources().getIdentifier(ball.getName() + "_ball_" + player.getPlayer(), "id", getPackageName());
+                savedInstanceState.putCharSequence(String.valueOf(id), getScoreBallTextView(ball.getName(), player.getPlayer()).getText());
+            }
+        }
+
+        for (Ball ball : Ball.values()) {
+            id = getResources().getIdentifier(ball.getName() + "_ball_btn", "id", getPackageName());
+            savedInstanceState.putCharSequence(String.valueOf(id), getBallBtnTextView(ball.getName()).getText());
+        }
+
+        savedInstanceState.putSerializable(CURRENT_PLAYER, currentPlayer);
+        savedInstanceState.putSerializable(CURRENT_POT_BALL, potBall);
+        savedInstanceState.putBoolean(COLOUR_SEQUENCE, colourSequence);
+        savedInstanceState.putSerializable(NEXT_BALL_COLOUR_SEQUENCE, nextBallColourSequence);
+        savedInstanceState.putInt(CURRENT_PLAYER_COLOUR, currentPlayerColour);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        int id;
+        CharSequence memorizeString;
+
+        for (Round player : Round.values()) {
+            id = getResources().getIdentifier(player.getPlayer() + "_score", "id", getPackageName());
+            memorizeString = savedInstanceState.getCharSequence(String.valueOf(id));
+            getScoreTextView(player.getPlayer()).setText(memorizeString);
+
+            if(player.getPlayer().equals("p1"))
+                scorePlayer1 = Integer.parseInt(memorizeString.toString());
+            else
+                scorePlayer2 = Integer.parseInt(memorizeString.toString());
+
+            id = getResources().getIdentifier(player.getPlayer() + "_foul", "id", getPackageName());
+            memorizeString = savedInstanceState.getCharSequence(String.valueOf(id));
+            getFoulsTextView(player.getPlayer()).setText(memorizeString);
+
+            for (Ball ball : Ball.values()) {
+                id = getResources().getIdentifier(ball.getName() + "_ball_" + player.getPlayer(), "id", getPackageName());
+                memorizeString = savedInstanceState.getCharSequence(String.valueOf(id));
+                getScoreBallTextView(ball.getName(), player.getPlayer()).setText(memorizeString);
+            }
+        }
+
+        for (Ball ball : Ball.values()) {
+            id = getResources().getIdentifier(ball.getName() + "_ball_btn", "id", getPackageName());
+            memorizeString = savedInstanceState.getCharSequence(String.valueOf(id));
+            getBallBtnTextView(ball.getName()).setText(memorizeString);
+
+            if(ball.getName().equals("red"))
+                redBall = Integer.parseInt(memorizeString.toString());
+        }
+
+        currentPlayer = (Round) savedInstanceState.getSerializable(CURRENT_PLAYER);
+        potBall = (Ball) savedInstanceState.getSerializable(CURRENT_POT_BALL);
+        colourSequence = savedInstanceState.getBoolean(COLOUR_SEQUENCE);
+        nextBallColourSequence = (Ball) savedInstanceState.getSerializable(NEXT_BALL_COLOUR_SEQUENCE);
+
+        currentPlayerDecorator(currentPlayerColour = savedInstanceState.getInt(CURRENT_PLAYER_COLOUR));
+    }
+
+    public void wrongShot(View v) {
+        displayWrongShotMatchStory();
+        changeRound(false);
+        activeColorSequence();
+    }
+
+    public void restartMatch(View v) {
+        for(Round player : Round.values()) {
+            display(Integer.parseInt(getString(R.string.start_value_score)), getScoreTextView(player.getPlayer()));
+            display(Integer.parseInt(getString(R.string.start_value_score)), getFoulsTextView(player.getPlayer()));
+
+            for(Ball ball : Ball.values())
+                display(Integer.parseInt(getString(R.string.start_value_score)), getScoreBallTextView(ball.getName(), player.getPlayer()));
+        }
+
+        for(Ball ball : Ball.values()) {
+            getBallBtnTextView(ball.getName()).setEnabled(true);
+
+            if (!ball.getName().equals("red"))
+                display(Integer.parseInt(getString(R.string.start_value_btn)), getBallBtnTextView(ball.getName()));
+            else
+                display(Integer.parseInt(getString(R.string.start_red_ball)), getBallBtnTextView(ball.getName()));
+        }
+
+        initializeState();
+    }
+
+    private void initializeState() {
+        redBall = MAX_RED_BALL;
+        scorePlayer1 = START_SCORE;
+        scorePlayer2 = START_SCORE;
+        nextBallColourSequence = null;
+        colourSequence = false;
+        currentPlayer = ((new Random()).nextInt(2) == 0) ? Round.PLAYER_1 : Round.PLAYER_2;
+        potBall = null;
+
+        playerStartStyle();
+        displayStartMatchStory();
     }
 
     public void redBallShot(View v) {
-        potBall = Ball.RED;
-
-        if(!isWrongShot()) {
-            redBall--;
-            display(redBall, (TextView) findViewById(R.id.red_ball_btn));
-            addScore(potBall.getPoint(), getScoreTextView(), getScoreBallTextView("red"));
+        if(isFoulShot(Ball.RED.getName())) {
+            manageFoul();
+            return;
         }
-        else
-            changeRound();
+
+        potBall = Ball.RED;
+        redBall--;
+
+        display(redBall, (TextView) findViewById(R.id.red_ball_btn));
+        addScore(potBall.getPoint(), getScoreTextView(), getScoreBallTextView(potBall.getName()));
+        displayPotBallMatchStory();
+
+        if(redBall == 0)
+            getBallBtnTextView(potBall.getName()).setEnabled(false);
     }
 
     public void colourBallShot(View v) {
-        String colourBall = null;
+        if(isFoulShot(Ball.BLACK.getName()) && !colourSequence) {
+            manageFoul();
+            return;
+        }
 
         switch(v.getId()) {
             case R.id.yellow_ball_btn:
-                colourBall = "yellow";
                 potBall = Ball.YELLOW;
                 break;
             case R.id.green_ball_btn:
-                colourBall = "green";
                 potBall = Ball.GREEN;
                 break;
             case R.id.brown_ball_btn:
-                colourBall = "brown";
                 potBall = Ball.BROWN;
                 break;
             case R.id.blue_ball_btn:
-                colourBall = "blue";
                 potBall = Ball.BLUE;
                 break;
             case R.id.pink_ball_btn:
-                colourBall = "pink";
                 potBall = Ball.PINK;
                 break;
             case R.id.black_ball_btn:
-                colourBall = "black";
                 potBall = Ball.BLACK;
                 break;
         }
 
-        if(!isWrongShot())
-            addScore(potBall.getPoint(), getScoreTextView(), getScoreBallTextView(colourBall));
-        else
-            changeRound();
+        displayPotBallMatchStory();
+
+        if(colourSequence) {
+            if(nextBallColourSequence != potBall)
+                manageFoul();
+            else {
+                display(0, getBallBtnTextView(potBall.getName()));
+                getBallBtnTextView(potBall.getName()).setEnabled(false);
+
+                addScore(potBall.getPoint(), getScoreTextView(), getScoreBallTextView(potBall.getName()));
+                nextBallColourSequence = Ball.nextColourBall(potBall);
+            }
+        }
+        else {
+            addScore(potBall.getPoint(), getScoreTextView(), getScoreBallTextView(potBall.getName()));
+            activeColorSequence();
+        }
+    }
+
+    private void activeColorSequence() {
+        if (redBall == 0 && !colourSequence) {
+            colourSequence = true;
+            nextBallColourSequence = Ball.YELLOW;
+        }
     }
 
     private void addScore(int point, TextView scoreTextView, TextView scoreBallTextView) {
-        int score = (getCurrentPlayer().equals(PLAYER_1)) ? (scorePlayer1 += point) : (scorePlayer2 += point);
+        int score = (getCurrentPlayer().equals(Round.PLAYER_1.getPlayer())) ? (scorePlayer1 += point) : (scorePlayer2 += point);
 
         display(score, scoreTextView);
-        display(Integer.parseInt(scoreBallTextView.getText().toString()) + 1, scoreBallTextView);
+
+        if(scoreBallTextView != null)
+            display(Integer.parseInt(scoreBallTextView.getText().toString()) + 1, scoreBallTextView);
     }
 
     private void display(int number, TextView v) {
         v.setText(String.valueOf(number));
     }
 
-    private boolean isWrongShot() {
-        return (new Random()).nextInt(6) == 0;
+    private boolean isFoulShot(String currentBall) {
+        if(currentBall.equals(Ball.RED.getName()) && potBall == Ball.RED)
+            return true;
+        if(!currentBall.equals(Ball.RED.getName()) && potBall != Ball.RED)
+            return true;
+
+        return false;
     }
 
-    private void changeRound() {
-        currentPlayerCancelDecoretor();
+    private void manageFoul() {
+        display(Integer.parseInt(getFoulsTextView().getText().toString()) + 1, getFoulsTextView());
+        addScore(FOUL, getScoreTextView(), null);
+        displayFoulMatchStory();
+        changeRound(true);
+    }
 
-        if(getCurrentPlayer().equals(PLAYER_1))
-            round = Round.PLAYER_2;
+    private void changeRound(boolean changeForFoul) {
+        potBall = null;
+        currentPlayerCancelDecorator();
+
+        if(getCurrentPlayer().equals(Round.PLAYER_1.getPlayer()))
+            currentPlayer = Round.PLAYER_2;
         else
-            round = Round.PLAYER_1;
+            currentPlayer = Round.PLAYER_1;
 
-        currentPlayerDecoretor();
+        if(changeForFoul)
+            currentPlayerDecorator(currentPlayerColour = getResources().getColor(R.color.playerFoul));
+        else
+            currentPlayerDecorator(currentPlayerColour = getResources().getColor(R.color.playerWrongShot));
     }
 
-    private void currentPlayerDecoretor() {
-        currentPlayerModifyStyle("#bbdefb", Typeface.DEFAULT_BOLD);
+    private void playerStartStyle() {
+        playerModifyStyle(getResources().getColor(R.color.white), Typeface.DEFAULT, Round.PLAYER_1.getPlayer());
+        playerModifyStyle(getResources().getColor(R.color.white), Typeface.DEFAULT, Round.PLAYER_2.getPlayer());
+        currentPlayerDecorator(currentPlayerColour = getResources().getColor(R.color.playerStartColor));
     }
 
-    private void currentPlayerCancelDecoretor() {
-        currentPlayerModifyStyle("#ffffff", Typeface.DEFAULT);
+    private void currentPlayerDecorator(int colour) {
+        playerModifyStyle(colour, Typeface.DEFAULT_BOLD, currentPlayer.getPlayer());
     }
 
-    private void currentPlayerModifyStyle(String colour, Typeface style) {
-        TextView playerText = findViewById(getResources().getIdentifier(getCurrentPlayer() + "_text", "id", getPackageName()));
+    private void currentPlayerCancelDecorator() {
+        playerModifyStyle(getResources().getColor(R.color.white), Typeface.DEFAULT, currentPlayer.getPlayer());
+    }
 
-        playerText.setTextColor(Color.parseColor(colour));
+    private void playerModifyStyle(int colour, Typeface style, String player) {
+        TextView playerText = findViewById(getResources().getIdentifier(player + "_text", "id", getPackageName()));
+
+        playerText.setTextColor(colour);
         playerText.setTypeface(style);
+    }
+
+    private TextView getFoulsTextView() {
+        return (TextView) findViewById(getResources().getIdentifier(getCurrentPlayer() + "_fouls", "id", getPackageName()));
+    }
+
+    private TextView getFoulsTextView(String player) {
+        return (TextView) findViewById(getResources().getIdentifier(player + "_fouls", "id", getPackageName()));
     }
 
     private TextView getScoreTextView() {
         return (TextView) findViewById(getResources().getIdentifier(getCurrentPlayer() + "_score", "id", getPackageName()));
     }
 
+    private TextView getScoreTextView(String player) {
+        return (TextView) findViewById(getResources().getIdentifier(player + "_score", "id", getPackageName()));
+    }
+
     private TextView getScoreBallTextView(String colourBall) {
         return (TextView) findViewById(getResources().getIdentifier(colourBall + "_ball_" + getCurrentPlayer(), "id", getPackageName()));
     }
 
+    private TextView getScoreBallTextView(String colourBall, String player) {
+        return (TextView) findViewById(getResources().getIdentifier(colourBall + "_ball_" + player, "id", getPackageName()));
+    }
+
+    private TextView getBallBtnTextView(String colourBall) {
+        return (TextView) findViewById(getResources().getIdentifier(colourBall + "_ball_btn", "id", getPackageName()));
+    }
+
     private String getCurrentPlayer() {
-        return (round == Round.PLAYER_1) ? PLAYER_1 : PLAYER_2;
+        return currentPlayer.getPlayer();
+    }
+
+    private void displayStartMatchStory() {
+        String player = (getCurrentPlayer().equals("p1")) ? "Player 1: " : "Player 2: ";
+        displayMatchStory(player + " start match");
+    }
+
+    private void displayPotBallMatchStory() {
+        displayMatchStory("pot " + potBall.getName() + " ball, +" + potBall.getPoint() + " point");
+    }
+
+    private void displayFoulMatchStory() {
+        displayMatchStory("foul " + FOUL + " point, change player!!");
+    }
+
+    private void displayWrongShotMatchStory() {
+        displayMatchStory("wrong shot, change player!!");
+    }
+
+    private void displayMatchStory(String story) {
+        ((TextView) findViewById(R.id.match_story)).setText(story);
     }
 }
